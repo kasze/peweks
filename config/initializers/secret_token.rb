b3aae8df71e65afae08659d9f417832d9f3c4800
Kamil Szewczyk <kamil.szewczyk@gmail.com>
2012-10-16 12:52:23 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peweks::Application.config.secret_token = '19e1f6cb2a1a3f293da209b754395082c02c4bd3c74321661d94e0419eb994e66690780db44bf794aff047da1d01432e9cc870c27c0f75697e824d751b6fa6ff'
